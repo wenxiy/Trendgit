6117029a141f87e899432d802fd0b48e81820dcc
wenxiy <193367189@qq.com>
2020-05-15 23:56:41 +0800

    debug

--- Trend/app/src/main/java/com/example/trend/ui/adapter/ListViewAdapter.java
+++ Trend/app/src/main/java/com/example/trend/ui/adapter/ListViewAdapter.java
@@ -6,9 +6,13 @@ import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.trend.R;
 import com.example.trend.service.entity.Developers;
 import java.util.List;
 import java.util.Objects;
+
+import static android.view.View.inflate;
 
 public class ListViewAdapter extends RecyclerView.Adapter<ListViewAdapter.InnerHolder> {
     private final List<Developers> mdeveloper;
@@ -20,22 +24,33 @@ public class ListViewAdapter extends RecyclerView.Adapter<ListViewAdapter.InnerH
     @NonNull
     @Override
     public InnerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        //传入条目
+    View view=View.inflate(parent.getContext(), R.layout.item_list,null);
+        return new InnerHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull InnerHolder holder, int position) {
-
+        //设置数据
+        holder.setData(mdeveloper.get(position));
     }
 
     @Override
     public int getItemCount() {
+        if (mdeveloper!=null)
+        {
+            return mdeveloper.size();
+        }
         return 0;
     }
 
     public class InnerHolder extends RecyclerView.ViewHolder {
         public InnerHolder(View itemView) {
             super(itemView);
+            
+        }
+        public void setData(Developers data){
+
         }
     }
 }
