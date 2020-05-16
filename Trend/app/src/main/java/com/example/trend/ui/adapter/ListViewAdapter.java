87a85986e7227c4605059b74c4018fa4b91f97d5
wenxiy <193367189@qq.com>
2020-05-16 13:07:45 +0800

    debug

--- Trend/app/src/main/java/com/example/trend/ui/adapter/ListViewAdapter.java
+++ Trend/app/src/main/java/com/example/trend/ui/adapter/ListViewAdapter.java
@@ -3,12 +3,15 @@ package com.example.trend.ui.adapter;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.trend.R;
 import com.example.trend.service.entity.Developers;
+import com.facebook.drawee.view.SimpleDraweeView;
+
 import java.util.List;
 import java.util.Objects;
 
@@ -45,12 +48,18 @@ public class ListViewAdapter extends RecyclerView.Adapter<ListViewAdapter.InnerH
     }
 
     public class InnerHolder extends RecyclerView.ViewHolder {
+        private SimpleDraweeView maver;
+        private TextView mitem_t1;
+        private TextView mitem_t2;
         public InnerHolder(View itemView) {
             super(itemView);
-            
+            maver=(SimpleDraweeView) itemView.findViewById(R.id.aver);
+            mitem_t1=(TextView)itemView.findViewById(R.id.item_t1);
+            mitem_t2=(TextView)itemView.findViewById(R.id.item_t2);
         }
         public void setData(Developers data){
-
+                maver.setImageURI(data.getAvatar());
+                mitem_t1.setText(data.);
         }
     }
 }
