16d0239c3a7505f87461cdd00ec6d733da55edc8
wenxiy <193367189@qq.com>
2020-08-01 20:54:23 +0800

    update

--- Trend/app/src/main/java/com/example/trend/service/Retrofit/RetrofitHelper.java
+++ Trend/app/src/main/java/com/example/trend/service/Retrofit/RetrofitHelper.java
@@ -5,6 +5,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitHelper {
+
     public static volatile RetrofitService service;
 
     public static RetrofitService getService() {
