3c4c16407c17bf39a8af5491e98461a23fa220a0
wenxiy <193367189@qq.com>
2025-04-20 18:48:25 +1000

    feats: add the floder in button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.coil.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.coil.network.okhttp)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/example/trend_compose/MainActivity.kt
+++ app/src/main/java/com/example/trend_compose/MainActivity.kt
@@ -4,31 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.clickable
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.LinkAnnotation
+
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.trend_compose.ui.theme.Trend_ComposeTheme
+import com.example.trend_compose.ui.theme.components.ProjectCard
+import com.example.trend_compose.viewmodel.ProjectViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,78 +35,47 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Trend_ComposeTheme {
-                Column(
-                    modifier = Modifier
-                        .padding(15.dp)
-                        .padding(
-                            top = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
-                        )
-                ) {
-                    Text(
-                        text = "\uD83D\uDCE6 GitHub 项目",
-                        style = MaterialTheme.typography.titleLarge
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    PorjectListScreen(
-                        modifier = Modifier
-                            .weight(1f)
-                            .fillMaxWidth()
-                    )
-                }
-            }
+                MainScreen()
 
+            }
         }
     }
 
     @Composable
-    fun ProjectCard(
-        name: String,
-        imageUrl: String,
-        onClick: () -> Unit
-    ) {
-        Card(
+    fun MainScreen() {
+        val vm: ProjectViewModel = viewModel()
+
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-                .clickable { onClick() },
-            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+                .padding(15.dp)
+                .padding(
+                    top = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
+                )
         ) {
-            Row(
-                modifier = Modifier.padding(16.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                AsyncImage(
-                    model = imageUrl,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(48.dp)
-                        .padding(end = 16.dp)
-                )
-                Text(
-                    text = name,
-                    style = MaterialTheme.typography.titleMedium,
-                    color = Color.Black
-                )
-            }
+            Text(
+                text = "\uD83D\uDCE6 GitHub 项目",
+                style = MaterialTheme.typography.titleLarge
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            PorjectListScreen(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                viewModel = vm
+            )
         }
     }
 
     @Composable
-    fun PorjectListScreen(modifier: Modifier) {
-        val fakeProjects = listOf(
-            "Compose-Gallery",
-            "JetNews",
-            "NowInAndroid",
-            "Kotlin-Coroutines",
-            "Awesome-Android-UI"
-        )
+    fun PorjectListScreen(modifier: Modifier, viewModel: ProjectViewModel) {
+        val projects by viewModel.projectList.collectAsState()
         LazyColumn(modifier = Modifier.padding(16.dp)) {
-            items(fakeProjects) { projectName ->
+            items(projects) { project ->
                 ProjectCard(
-                    name = projectName,
-                    imageUrl = "https://avatars.githubusercontent.com/u/9919",
+                    name = project.name,
+                    imageUrl = project.imageUrl,
                     onClick = {
-                        println("点击了 $projectName")
+                        println("点击了 ${project.name}")
                     }
                 )
             }
